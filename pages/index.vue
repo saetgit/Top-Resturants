5634a4c9138f9a4dc82e3a62fcd965653eff28f1
saetgit <etemadian.sara.1998@gmail.com>
2024-04-30 09:32:06 +0330

    create layout error

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,26 @@
 <template>
-    <div>
-      <div class="container">
-        <h1>Welcome to Restaurantly</h1>
-        <NuxtLink to="/restaurants">Go to restaurants</NuxtLink>
-      </div>
+  <div>
+    <Html :lang="dynamic > 50 ? 'en-GB' : 'en-US'">
+      <Head>
+        <Title>Restaurantly </Title>
+        <Meta name="description" content="My page is cool" />
+        <Link
+          rel="preload"
+          href="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+          as="script"
+        />
+      </Head>
+    </Html>
+    <div class="container">
+      <h1>Welcome to Restaurantly</h1>
+      <NuxtLink to="/restaurants">Go to restaurants</NuxtLink>
     </div>
-  </template>
-  
-  <style scoped>
-  .container {
-    text-align: center;
-    margin-top: 5rem;
-  }
-  </style>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
